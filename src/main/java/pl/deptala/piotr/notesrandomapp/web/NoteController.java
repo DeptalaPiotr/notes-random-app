7ca1e3cff291819b87385b87773595f57652bfcb
Piotr <deptala.piotr@gmail.com>
2023-01-05 20:08:57 +0100

    add CRUD template

--- src/main/java/pl/deptala/piotr/notesrandomapp/web/NoteController.java
+++ src/main/java/pl/deptala/piotr/notesrandomapp/web/NoteController.java
@@ -1,4 +1,37 @@
 package pl.deptala.piotr.notesrandomapp.web;
 
+import java.util.logging.Logger;
+
 public class NoteController {
+    private static final Logger LOGGER = Logger.getLogger(NoteController.class.getName());
+
+    // C - create
+    public void create() {
+        LOGGER.info("create()");
+        LOGGER.info("create(...)");
+    }
+
+    // R - read
+    public void read() {
+        LOGGER.info("read()");
+        LOGGER.info("read(...)");
+    }
+
+    // U - update
+    public void update() {
+        LOGGER.info("update()");
+        LOGGER.info("update(...)");
+    }
+
+    // D - delete
+    public void delete() {
+        LOGGER.info("delete()");
+        LOGGER.info("delete(...)");
+    }
+
+    // L - list
+    public void list() {
+        LOGGER.info("list()");
+        LOGGER.info("list(...)");
+    }
 }
