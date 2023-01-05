f621eee95aa0b6ef8cc8b623dfcef995bb2c3f19
deptala <deptala.piotr@gmail.com>
2023-01-05 19:59:15 +0100

    creation of the Note model

--- src/main/java/pl/deptala/piotr/notesrandomapp/web/model/NoteModel.java
+++ src/main/java/pl/deptala/piotr/notesrandomapp/web/model/NoteModel.java
@@ -1,4 +1,50 @@
 package pl.deptala.piotr.notesrandomapp.web.model;
 
 public class NoteModel {
+
+    private long id;
+    private String title;
+    private String text;
+
+    public NoteModel() {
+    }
+
+    public NoteModel(long id, String title, String text) {
+        this.id = id;
+        this.title = title;
+        this.text = text;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "NoteModel{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", text='" + text + '\'' +
+                '}';
+    }
 }
